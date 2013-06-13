e7c3a35e8d82d063a882af948a1563771b83144a
Jiuding Duan <jiuding.duan@gmail.com>
2013-06-14 01:34:20 +0800

    joint distribution OK

--- brml/potential/potential.py
+++ brml/potential/potential.py
@@ -16,15 +16,20 @@ class potential:
                 newpot = copy.copy(self)
 #FIX ME: dimension consistency not checked
 #FIX ME: only 1-D multiply considered
+                print "NOT-SWAPED table: \n", newpot.table
 		commonitem = np.intersect1d(np.int8(self.variables),np.int8(other.variables))
 		commonindex = np.int8(np.in1d(self.variables,commonitem).nonzero())
 		commonshape = np.array(self.table.shape)[commonindex]
 		newpot.variables[0],newpot.variables[commonindex] = newpot.variables[commonindex],newpot.variables[0]
+                print "current commonindex:", commonindex
+                print "current commonitem:", commonitem
+                print "current commonshape:", commonshape
 		print "current variable index:", newpot.variables
-		newpot.table.swapaxes(commonindex,0)
+		newpot.table=newpot.table.swapaxes(commonindex,0)
+                print "SWAPED table: \n", newpot.table
 		for i in range(commonshape):
 		#FIX ME: only swap (1-D case) the axes1=commonindex and axes2=0
 			newpot.table[i,...] = newpot.table[i,...] * other.table[i]
 			#newpot.table[i,...] = table * other.table
-			
+		print "current table: \n", newpot.table
 		return newpot
